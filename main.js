58ccd6ebdc5776af771891ebde2545fd347e9e95
st-stratus <89566675+st-stratus@users.noreply.github.com>
2022-11-22 08:30:46 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
 const game = document.querySelector(".gm")
-const game.width = window.innerWidth;
+const width = game.width = window.innerWidth;
+const height = game.height = window.innerHeight;
+const ctx = game.getContext("2d");
+ctx.fillStyle = "rgb(0, 0, 0)";
+ctx.fillRect(0, 0, width, height);
